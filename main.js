05772ff3ee2366fa1d896aba9d46d0c939748e0b
pdbjjens <jjensen@t-online.de>
2020-02-11 18:26:27 +0100

    ctx Connection()

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ const _ = require('lodash');
 // Variable definitions
 const spec = vbus.Specification.getDefaultSpecification();
 const ctx = {
-    headerSet: vbus.HeaderSet,
-    hsc: vbus.HeaderSetConsolidator,
-    connection: vbus.Connection
+    headerSet: vbus.HeaderSet(),
+    hsc: vbus.HeaderSetConsolidator(),
+    connection: vbus.Connection()
 };
 
 class MyVbus extends utils.Adapter {
