4629b083e6259f653699fa6a2664eb05e48a4082
pdbjjens <jjensen@t-online.de>
2020-02-13 14:30:40 +0100

    .de in common

--- main.js
+++ main.js
@@ -222,7 +222,7 @@ class MyVbus extends utils.Adapter {
                 native: {}
             });
             const common = {
-                name: item.name,
+                name: item.name.de,
                 type: 'number',
                 unit: item.unitText,
                 read: true,
