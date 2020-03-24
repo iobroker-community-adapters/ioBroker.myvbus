2099d814df36f1f1d55b0054c24b84e78a1c1840
pdbjjens <jjensen@t-online.de>
2020-03-24 11:25:04 +0100

    Add translations to words.js

--- admin/words.js
+++ admin/words.js
@@ -4,102 +4,302 @@
 systemDictionary = {
     'Connection Type': {
         'en': 'Connection Type',
-        'de': 'Verbindungstyp'
+        'de': 'Verbindungstyp',
+        'ru': 'Тип соединения',
+        'pt': 'Tipo de conexão',
+        'nl': 'Connectie type',
+        'fr': 'Type de connexion',
+        'it': 'Tipo di connessione',
+        'es': 'Tipo de conección',
+        'pl': 'Rodzaj połączenia',
+        'zh-cn': '连接类型'
     },
     'Select VBus Connection Device Type': {
         'en': 'Select VBus Connection Device Type (VBus/LAN, DL3 etc.)',
-        'de': 'VBus Verbindungsgerätetyp auswählen (VBus/LAN, DL3 usw.)'
+        'de': 'Wählen Sie den VBus-Verbindungsgerätetyp (VBus / LAN, DL3 usw.).',
+        'ru': 'Выберите тип устройства подключения VBus (VBus / LAN, DL3 и т. Д.)',
+        'pt': 'Selecione o tipo de dispositivo de conexão VBus (VBus / LAN, DL3 etc.)',
+        'nl': 'Selecteer het type VBus-verbindingsapparaat (VBus / LAN, DL3 enz.)',
+        'fr': 'Sélectionnez le type de périphérique de connexion VBus (VBus / LAN, DL3, etc.)',
+        'it': 'Seleziona il tipo di dispositivo di connessione VBus (VBus / LAN, DL3 ecc.)',
+        'es': 'Seleccione el tipo de dispositivo de conexión VBus (VBus / LAN, DL3, etc.)',
+        'pl': 'Wybierz typ urządzenia podłączeniowego VBus (VBus / LAN, DL3 itp.)',
+        'zh-cn': '选择VBus连接设备类型（VBus / LAN，DL3等）'
     },
     'VBus/USB': {
         'en': 'VBus/USB',
-        'de': 'VBus/USB'
+        'de': 'VBus / USB',
+        'ru': 'Vbus / USB',
+        'pt': 'VBus / USB',
+        'nl': 'VBus / USB',
+        'fr': 'VBus / USB',
+        'it': 'VBus / USB',
+        'es': 'VBus / USB',
+        'pl': 'VBus / USB',
+        'zh-cn': 'VBus / USB'
     },
     'Serial/LANGW': {
         'en': 'Serial/LANGW',
-        'de': 'Seriell/LANGW'
+        'de': 'Seriell / LANGW',
+        'ru': 'Последовательный / LANGW',
+        'pt': 'Serial / LANGW',
+        'nl': 'Serieel / LANGW',
+        'fr': 'Série / LANGW',
+        'it': 'Serial / LANGW',
+        'es': 'Serie / LANGW',
+        'pl': 'Szeregowy / LANGW',
+        'zh-cn': '串口/ LANGW'
     },
     'VBus/LAN': {
         'en': 'VBus/LAN',
-        'de': 'VBus/LAN'
+        'de': 'VBus / LAN',
+        'ru': 'Vbus / LAN',
+        'pt': 'VBus / LAN',
+        'nl': 'VBus / LAN',
+        'fr': 'VBus / LAN',
+        'it': 'VBus / LAN',
+        'es': 'VBus / LAN',
+        'pl': 'VBus / LAN',
+        'zh-cn': 'VBus /局域网'
     },
     'DL2/KM2': {
         'en': 'DL2/KM2',
-        'de': 'DL2/KM2'
+        'de': 'DL2 / KM2',
+        'ru': 'DL2 / КМ2',
+        'pt': 'DL2 / KM2',
+        'nl': 'DL2 / KM2',
+        'fr': 'DL2 / KM2',
+        'it': 'DL2 / KM2',
+        'es': 'DL2 / KM2',
+        'pl': 'DL2 / KM2',
+        'zh-cn': 'DL2 / KM2'
     },
     'DL3': {
         'en': 'DL3',
-        'de': 'DL3'
+        'de': 'DL3',
+        'ru': 'DL3',
+        'pt': 'DL3',
+        'nl': 'DL3',
+        'fr': 'DL3',
+        'it': 'DL3',
+        'es': 'DL3',
+        'pl': 'DL3',
+        'zh-cn': 'DL3'
     },
     'Connection Identifier': {
         'en': 'Connection Identifier',
-        'de': 'Verbindungskennung'
+        'de': 'Verbindungskennung',
+        'ru': 'Идентификатор соединения',
+        'pt': 'Identificador de conexão',
+        'nl': 'Verbindingsidentificatie',
+        'fr': 'Identifiant de connexion',
+        'it': 'Identificatore di connessione',
+        'es': 'Identificador de conexión',
+        'pl': 'Identyfikator połączenia',
+        'zh-cn': '连接标识符'
     },
     'IP address for local LAN or URL for VBus.net or Port for VBus/USB access': {
         'en': 'IP address for local LAN or URL for VBus.net or Port for VBus/USB access',
-        'de': 'IP Addresse für lokalen LAN Zugang oder URL für VBus.net oder Port für VBus/USB'
+        'de': 'IP-Adresse für lokales LAN oder URL für VBus.net oder Port für VBus / USB-Zugriff',
+        'ru': 'IP-адрес для локальной сети или URL-адрес для VBus.net или порт для доступа к VBus / USB',
+        'pt': 'Endereço IP para LAN local ou URL para VBus.net ou Porta para acesso VBus / USB',
+        'nl': 'IP-adres voor lokaal LAN of URL voor VBus.net of poort voor VBus / USB-toegang',
+        'fr': 'Adresse IP pour LAN local ou URL pour VBus.net ou port pour accès VBus / USB',
+        'it': 'Indirizzo IP per LAN locale o URL per VBus.net o Porta per accesso VBus / USB',
+        'es': 'Dirección IP para LAN local o URL para VBus.net o Puerto para acceso VBus / USB',
+        'pl': 'Adres IP dla lokalnej sieci LAN lub URL dla VBus.net lub Port dla dostępu VBus / USB',
+        'zh-cn': '本地LAN的IP地址或VBus.net的URL或VBus / USB访问的端口'
     },
     'VBus Password': {
         'en': 'VBus Password',
-        'de': 'VBus Passwort'
+        'de': 'VBus Passwort',
+        'ru': 'VBus Пароль',
+        'pt': 'Senha do VBus',
+        'nl': 'VBus-wachtwoord',
+        'fr': 'Mot de passe VBus',
+        'it': 'Password VBus',
+        'es': 'Contraseña de VBus',
+        'pl': 'Hasło VBus',
+        'zh-cn': 'VBus密码'
     },
     'Default: vbus  for LAN or admin for DLx': {
         'en': 'Default: vbus  for LAN or admin for DLx',
-        'de': 'Standard: vbus für LAN, admin für DLx'
+        'de': 'Standard: vbus für LAN oder admin für DLx',
+        'ru': 'По умолчанию: vbus для локальной сети или администратор для DLx',
+        'pt': 'Padrão: vbus para LAN ou admin para DLx',
+        'nl': 'Standaard: vbus voor LAN of admin voor DLx',
+        'fr': 'Par défaut: vbus pour LAN ou admin pour DLx',
+        'it': 'Predefinito: vbus per LAN o admin per DLx',
+        'es': 'Valor predeterminado: vbus para LAN o administrador para DLx',
+        'pl': 'Domyślnie: vbus dla LAN lub admin dla DLx',
+        'zh-cn': '默认值：用于局域网的vbus或用于DLx的管理员'
     },
     'DL3 Channel 1': {
         'en': 'DL3 Channel 1',
-        'de': 'DL3 Kanal 1'
+        'de': 'DL3-Kanal 1',
+        'ru': 'DL3 канал 1',
+        'pt': 'Canal DL3 1',
+        'nl': 'DL3 kanaal 1',
+        'fr': 'DL3 Channel 1',
+        'it': 'Canale DL3 1',
+        'es': 'DL3 Canal 1',
+        'pl': 'Kanał DL3 1',
+        'zh-cn': 'DL3频道1'
     },
     'DL3 Channel 2': {
         'en': 'DL3 Channel 2',
-        'de': 'DL3 Kanal 2'
+        'de': 'DL3-Kanal 2',
+        'ru': 'DL3 канал 2',
+        'pt': 'Canal DL3 2',
+        'nl': 'DL3 kanaal 2',
+        'fr': 'DL3 Channel 2',
+        'it': 'Canale DL3 2',
+        'es': 'DL3 Canal 2',
+        'pl': 'Kanał DL3 2',
+        'zh-cn': 'DL3频道2'
     },
     'DL3 Channel 3': {
         'en': 'DL3 Channel 3',
-        'de': 'DL3 Kanal 3'
+        'de': 'DL3-Kanal 3',
+        'ru': 'DL3 канал 3',
+        'pt': 'Canal DL3 3',
+        'nl': 'DL3 kanaal 3',
+        'fr': 'DL3 Channel 3',
+        'it': 'Canale DL3 3',
+        'es': 'DL3 Canal 3',
+        'pl': 'Kanał DL3 3',
+        'zh-cn': 'DL3频道3'
     },
     'DL3 Channel 4': {
         'en': 'DL3 Channel 4',
-        'de': 'DL3 Kanal 4'
+        'de': 'DL3-Kanal 4',
+        'ru': 'DL3 канал 4',
+        'pt': 'Canal DL3 4',
+        'nl': 'DL3 kanaal 4',
+        'fr': 'DL3 Channel 4',
+        'it': 'Canale DL3 4',
+        'es': 'DL3 Canal 4',
+        'pl': 'Kanał DL3 4',
+        'zh-cn': 'DL3频道4'
     },
     'DL3 Channel 5': {
         'en': 'DL3 Channel 5',
-        'de': 'DL3 Kanal 5'
+        'de': 'DL3-Kanal 5',
+        'ru': 'DL3 канал 5',
+        'pt': 'Canal DL3 5',
+        'nl': 'DL3 kanaal 5',
+        'fr': 'DL3 Channel 5',
+        'it': 'Canale DL3 5',
+        'es': 'DL3 Canal 5',
+        'pl': 'Kanał DL3 5',
+        'zh-cn': 'DL3频道5'
     },
     'DL3 Channel 6': {
         'en': 'DL3 Channel 6',
-        'de': 'DL3 Kanal 6'
+        'de': 'DL3-Kanal 6',
+        'ru': 'DL3 канал 6',
+        'pt': 'Canal DL3 6',
+        'nl': 'DL3 kanaal 6',
+        'fr': 'DL3 Channel 6',
+        'it': 'Canale DL3 6',
+        'es': 'DL3 Canal 6',
+        'pl': 'Kanał DL3 6',
+        'zh-cn': 'DL3频道6'
     },
     'DL3 Channel 0': {
         'en': 'DL3 Channel 0',
-        'de': 'DL3 Kanal 0'
+        'de': 'DL3-Kanal 0',
+        'ru': 'DL3 канал 0',
+        'pt': 'Canal DL3 0',
+        'nl': 'DL3 kanaal 0',
+        'fr': 'DL3 Channel 0',
+        'it': 'Canale DL3 0',
+        'es': 'DL3 Canal 0',
+        'pl': 'Kanał DL3 0',
+        'zh-cn': 'DL3频道0'
     },
     'DL3 Channel': {
         'en': 'DL3 Channel',
-        'de': 'DL3 Kanal'
+        'de': 'DL3-Kanal',
+        'ru': 'Канал DL3',
+        'pt': 'Canal DL3',
+        'nl': 'DL3-kanaal',
+        'fr': 'Canal DL3',
+        'it': 'Canale DL3',
+        'es': 'Canal DL3',
+        'pl': 'Kanał DL3',
+        'zh-cn': 'DL3频道'
     },
     'Required for DL3 only': {
         'en': 'Required for DL3 only',
-        'de': 'Nur für DL3 nötig'
+        'de': 'Nur für DL3 erforderlich',
+        'ru': 'Требуется только для DL3',
+        'pt': 'Necessário apenas para DL3',
+        'nl': 'Alleen vereist voor DL3',
+        'fr': 'Requis pour DL3 uniquement',
+        'it': 'Richiesto solo per DL3',
+        'es': 'Requerido solo para DL3',
+        'pl': 'Wymagane tylko dla DL3',
+        'zh-cn': '仅DL3需要'
     },
     'Via tag': {
-        'en': 'VBus Update Interval (in seconds)',
-        'de': 'Via-Kennung'
+        'en': 'Via tag',
+        'de': 'Via-Kennung',
+        'ru': 'Через тег',
+        'pt': 'Via tag',
+        'nl': 'Via tag',
+        'fr': 'Via tag',
+        'it': 'Tramite tag',
+        'es': 'Vía etiqueta',
+        'pl': 'Poprzez tag',
+        'zh-cn': '通过标签'
     },
     'Required only for VBus.net access via DLx/KMx': {
         'en': 'Required only for VBus.net access via DLx/KMx',
-        'de': 'Nur für VBus.net Zugang über DLx/KMx nötig'
+        'de': 'Nur für den VBus.net-Zugriff über DLx / KMx erforderlich',
+        'ru': 'Требуется только для доступа к VBus.net через DLx / KMx',
+        'pt': 'Necessário apenas para acesso ao VBus.net via DLx / KMx',
+        'nl': 'Alleen vereist voor toegang tot VBus.net via DLx / KMx',
+        'fr': "Requis uniquement pour l'accès à VBus.net via DLx / KMx",
+        'it': "Richiesto solo per l'accesso VBus.net tramite DLx / KMx",
+        'es': 'Se requiere solo para el acceso a VBus.net a través de DLx / KMx',
+        'pl': 'Wymagane tylko dla dostępu VBus.net przez DLx / KMx',
+        'zh-cn': '仅通过DLx / KMx访问VBus.net时才需要'
     },
     'VBus Interval': {
         'en': 'VBus Update Interval (in seconds)',
-        'de': 'VBus Update Intervall (in Sekunden)'
+        'de': 'VBus-Aktualisierungsintervall (in Sekunden)',
+        'ru': 'Интервал обновления VBus (в секундах)',
+        'pt': 'Intervalo de atualização do VBus (em segundos)',
+        'nl': 'VBus Update-interval (in seconden)',
+        'fr': 'Intervalle de mise à jour VBus (en secondes)',
+        'it': 'Intervallo di aggiornamento VBus (in secondi)',
+        'es': 'Intervalo de actualización de VBus (en segundos)',
+        'pl': 'Interwał aktualizacji VBus (w sekundach)',
+        'zh-cn': 'VBus更新间隔（以秒为单位）'
     },
     'If Force Adapter Reinitialization is set, the adapter restarts with new configuration immediately on save': {
         'en': 'If Force Adapter Reinitialization is set, the adapter restarts with new configuration immediately on save',
-        'de': 'Wenn Adapter Neustart gesetzt ist, startet der Adapter beim Speichern sofort neu'
+        'de': 'Wenn Adapter Reinitialization gesetzt ist, wird der Adapter sofort nach dem Speichern mit einer neuen Konfiguration neu gestartet',
+        'ru': 'Если установлена принудительная повторная инициализация адаптера, адаптер перезапускается с новой конфигурацией сразу после сохранения.',
+        'pt': 'Se Forçar reinicialização do adaptador estiver definido, o adaptador será reiniciado com a nova configuração imediatamente ao salvar',
+        'nl': 'Als Force Adapter Reinitialization is ingesteld, wordt de adapter onmiddellijk opnieuw opgestart met een nieuwe configuratie',
+        'fr': "Si Forcer la réinitialisation de l'adaptateur est défini, l'adaptateur redémarre avec la nouvelle configuration immédiatement lors de l'enregistrement",
+        'it': "Se si imposta Reinizializzazione forzata adattatore, l'adattatore si riavvia con una nuova configurazione immediatamente al momento del salvataggio",
+        'es': 'Si se establece Forzar reinicialización del adaptador, el adaptador se reinicia con una nueva configuración inmediatamente al guardar',
+        'pl': 'Jeśli ustawiona jest opcja Wymuś ponowne zainicjowanie adaptera, adapter ponownie uruchamia się z nową konfiguracją natychmiast po zapisaniu',
+        'zh-cn': '如果设置了“强制适配器重新初始化”，则适配器在保存时立即以新配置重新启动'
     },
     'Force Adapter Reinitialization': {
         'en': 'Force Adapter Reinitialization',
-        'de': 'Erzwinge Adapter Neustart'
+        'de': 'Reinitialisierung des Adapters erzwingen',
+        'ru': 'Реинициализация силового адаптера',
+        'pt': 'Forçar reinicialização do adaptador',
+        'nl': 'Forceer opnieuw initialiseren van adapter',
+        'fr': "Forcer la réinitialisation de l'adaptateur",
+        'it': "Reinizializzazione dell'adattatore di forza",
+        'es': 'Reinicialización del adaptador de fuerza',
+        'pl': 'Wymuś ponowne zainicjowanie adaptera',
+        'zh-cn': '强制适配器重新初始化'
     }
 };
