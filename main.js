ac0af3dacecba5d745086a8deac36d9133fde98b
pdbjjens <jjensen@t-online.de>
2020-05-07 17:22:24 +0200

    no textValue conversion

--- main.js
+++ main.js
@@ -277,8 +277,8 @@ class MyVbus extends utils.Adapter {
                     const objectId = channelId + '.' + item.id.replace(/_/g, '');
                     const noneUnit = spec.getUnitById('None');
                     const numberType = spec.getTypeById('Number');
-                    const numberValue = spec.formatTextValueFromRawValueInternal(item.rawValue, noneUnit, item.rootTypeId, item.precision, noneUnit);
-                    const value = +numberValue;
+                    const textValue = spec.formatTextValueFromRawValueInternal(item.rawValue, noneUnit, item.rootTypeId, item.precision, noneUnit);
+                    const value = textValue;
                     if (forceReInit) {
                         this.initDevice(deviceId, channelId, objectId, item);
                     }
