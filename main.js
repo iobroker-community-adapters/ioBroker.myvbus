9685939df7265238eeb170881bcefdf7ad528ba3
pdbjjens <jjensen@t-online.de>
2021-05-06 18:16:18 +0200

    remove await info.connection

--- main.js
+++ main.js
@@ -202,9 +202,9 @@ class MyVbus extends utils.Adapter {
                 this.log.debug('Connection state changed to ' + connectionState);
                 if (connectionState === 'CONNECTED') {
                     this.log.info('Connection established');
-                    await this.setStateAsync('info.connection', true, true);
+                    this.setStateAsync('info.connection', true, true);
                 } else {
-                    await this.setStateAsync('info.connection', false, true);
+                    this.setStateAsync('info.connection', false, true);
                 }
             });
             ctx.headerSet = new vbus.HeaderSet();
